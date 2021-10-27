caa3c71e2cabe9f5f6a33ef0d1e3776a3a953309
Plantier <mlplantier@labs.neit.edu>
2021-10-27 16:56:31 -0400

    actually done maybe

--- W3/bio3riddles.js
+++ W3/bio3riddles.js
@@ -7,6 +7,9 @@ var store4 = "a can't opener"
 var store5 = "your age"
 var store6 = "a bank"
 var store7 = "a piano"
+var store8 = "a staircase"
+var store9 = "a rubber band"
+var store10 = "short"
 
 function answer1() {
     document.getElementById("question1").innerHTML=store1;
@@ -28,4 +31,13 @@ function answer6() {
 }
 function answer7() {
     document.getElementById("question7").innerHTML=store7;
+}
+function answer8() {
+    document.getElementById("question8").innerHTML=store8;
+}
+function answer9() {
+    document.getElementById("question9").innerHTML=store9;
+}
+function answer10() {
+    document.getElementById("question10").innerHTML=store10;
 }
